9ab383d65dcb194da97cd8667dec05f692094917
Rob Pupo <pupo.robert@gmail.com>
2021-06-14 18:28:59 -0400

    post funcitonality added to notes html

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -26,11 +26,12 @@ module.exports = (app) => {
   // Then the server saves the data to the tableData array)
   // ---------------------------------------------------------------------------
 
-  app.post('/api/tables', (req, res) => {
+  app.post('/api/notes', (req, res) => {
     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
     // It will do this by sending out the value "true" have a table
     // req.body is available since we're using the body parsing middleware
     console.log(req.body)
+    res.json(true);
 
     // if (tableData.length < 5) {
     //   tableData.push(req.body);
