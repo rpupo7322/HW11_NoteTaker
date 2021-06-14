47804ce8973eb0e751d8d5566ae47c7437de1244
Rob Pupo <pupo.robert@gmail.com>
2021-06-14 17:59:30 -0400

    changing routes files to conform to app

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,8 +2,8 @@
 // We are linking our routes to a series of "data" sources.
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 
-const tableData = require('../data/tableData');
-const waitListData = require('../data/waitinglistData');
+const database = require('../db/db.json');
+// const waitListData = require('../data/waitinglistData');
 
 // ROUTING
 
@@ -14,9 +14,9 @@ module.exports = (app) => {
   // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
 
-  app.get('/api/tables', (req, res) => res.json(tableData));
+  app.get('/api/notes', (req, res) => res.json(database));
 
-  app.get('/api/waitlist', (req, res) => res.json(waitListData));
+  // app.get('/api/waitlist', (req, res) => res.json(waitListData));
 
   // API POST Requests
   // Below code handles when a user submits a form and thus submits data to the server.
@@ -30,23 +30,25 @@ module.exports = (app) => {
     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
     // It will do this by sending out the value "true" have a table
     // req.body is available since we're using the body parsing middleware
-    if (tableData.length < 5) {
-      tableData.push(req.body);
-      res.json(true);
-    } else {
-      waitListData.push(req.body);
-      res.json(false);
-    }
+    console.log(req.body)
+
+    // if (tableData.length < 5) {
+    //   tableData.push(req.body);
+    //   res.json(true);
+    // } else {
+    //   waitListData.push(req.body);
+    //   res.json(false);
+    // }
   });
 
   // I added this below code so you could clear out the table while working with the functionality.
   // Don"t worry about it!
 
-  app.post('/api/clear', (req, res) => {
-    // Empty out the arrays of data
-    tableData.length = 0;
-    waitListData.length = 0;
+//   app.post('/api/clear', (req, res) => {
+//     // Empty out the arrays of data
+//     tableData.length = 0;
+//     waitListData.length = 0;
 
-    res.json({ ok: true });
-  });
+//     res.json({ ok: true });
+//   });
 };
